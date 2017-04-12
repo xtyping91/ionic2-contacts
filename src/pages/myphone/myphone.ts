7087f1266a19ef93456b9b6a1f46be88da2ae994
xtyping91 <xtyping@naver.com>
2017-04-12 19:24:41 +0900

    접속 기기 핸드폰 정보 추출

--- src/pages/myphone/myphone.ts
+++ src/pages/myphone/myphone.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Sim } from '@ionic-native/sim';
 
 @Component({
   selector: 'page-myphone',
-  templateUrl: 'myphone.html'
+  templateUrl: 'myphone.html',
+  providers: [Sim]
 })
 export class MyPhone {
+  simInfo: any;
+  constructor(private sim: Sim, public navCtrl: NavController) {
+    this.sim.getSimInfo().then(
+      (info) => { this.simInfo = info; console.log('Sim info: ', info); },
+      (err) => console.log('Unable to get sim info: ', err)
+    );
 
-  constructor(public navCtrl: NavController) {
+    this.sim.hasReadPermission().then(
+      (info) => console.log('Has permission: ', info)
+    );
 
+    this.sim.requestReadPermission().then(
+      () => console.log('Permission granted'),
+      () => console.log('Permission denied')
+    );
   }
-
 }
